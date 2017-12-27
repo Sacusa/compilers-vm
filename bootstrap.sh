376673c4bd82576d6daebfa40cd4265db978e9bc
Sudhanshu Gupta <Sacusa@users.noreply.github.com>
2017-12-27 12:37:04 +0530

    Update bootstrap.sh
    Update the link to 'student-dist.tar.gz'. Install the distribution files to 'usr/class/cs143/cool'. Update the symbol to '/home/vagrant/cool'.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,10 @@
 sudo apt-get install -y flex bison build-essential csh openjdk-6-jdk libxaw7-dev
 sudo mkdir /usr/class
+sudo mkdir /usr/class/cs143
+sudo mkdir /usr/class/cs143/cool
 sudo chown vagrant /usr/class
-cd /usr/class
-wget http://spark-university.s3.amazonaws.com/stanford-compilers/vm/student-dist.tar.gz
+cd /usr/class/cs143/cool
+wget https://s3-us-west-1.amazonaws.com/prod-edx/Compilers/Misc/student-dist.tar.gz
 tar -xf student-dist.tar.gz
-ln -s /usr/class/cs143/cool /home/vagrant
+ln -s /usr/class/cs143/cool /home/vagrant/cool
 echo "export PATH=/usr/class/cs143/cool/bin:\$PATH" >> /home/vagrant/.bashrc
